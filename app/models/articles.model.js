8edc75ed4fad0e153b76a5fa23b4dbd6b98be7c6
kevpstephens <kevpstephenson@gmail.com>
2025-04-30 13:17:08 +0100

    Made changes to code with feedback (from 6-POST) taken into account

--- app/models/articles.model.js
+++ app/models/articles.model.js
@@ -1,76 +1,76 @@
 const db = require("../../db/connection.js");
 
 exports.selectArticleById = async (article_id) => {
-    const queryStr = `
-      SELECT * FROM articles 
-      WHERE article_id = $1
-      `;
-  
-    const result = await db.query(queryStr, [article_id]);
-  
-    if (!result.rows.length) {
-      throw {
-        status: 404,
-        msg: `Article not found!`,
-      };
-    }
-    return result.rows[0];
-  };
-  
-  exports.selectAllArticles = async (sort_by = "created_at", order = "desc") => {
-    const validSortByColumns = ["author", "title", "article_id", "topic", "created_at", "votes", "article_img_url", "comment_count"]
-    const validSortByOrders = ["asc", "desc"]
+  const queryStr = `
+    SELECT * FROM articles 
+    WHERE article_id = $1
+    `;
 
-    if (!validSortByColumns.includes(sort_by)) {
-      throw {
-        status: 400,
-        msg: "Invalid sort_by column!"
-      }
-    }
+  const result = await db.query(queryStr, [article_id]);
 
-    if (!validSortByOrders.includes(order)) {
-      throw {
-        status: 400,
-        msg: "Invalid order_by value!"
-      }
-    }
-    
-    const queryStr = `
-          SELECT 
-              articles.author, 
-              articles.title, 
-              articles.article_id, 
-              articles.topic, 
-              articles.created_at, 
-              articles.votes, 
-              articles.article_img_url,
-              COUNT(comments.comment_id)::INT AS comment_count
-          FROM articles
-          LEFT JOIN comments
-          ON articles.article_id = comments.article_id
-          GROUP BY articles.article_id
-          ORDER BY ${sort_by} ${order.toUpperCase()};
-          `;
-  
-    const result = await db.query(queryStr);
-    return result.rows;
-  };
+  if (!result.rows.length) {
+    throw {
+      status: 404,
+      msg: `Article not found!`,
+    };
+  }
+  return result.rows[0];
+};
 
-  exports.updateArticleById = async (inc_votes, article_id) => {
-    const queryStr = `
-          UPDATE articles
-          SET votes = votes + $1
-          WHERE article_id = $2
-          RETURNING *;
-          `;
-  
-    const result = await db.query(queryStr, [inc_votes, article_id]);
-  
-    if (!result.rows.length) {
-      throw {
-        status: 404,
-        msg: `Article not found!`,
-      };
+exports.selectAllArticles = async (sort_by = "created_at", order = "desc") => {
+  const validSortByColumns = ["author", "title", "article_id", "topic", "created_at", "votes", "article_img_url", "comment_count"]
+  const validSortByOrders = ["asc", "desc"]
+
+  if (!validSortByColumns.includes(sort_by)) {
+    throw {
+      status: 400,
+      msg: "Invalid sort_by column!"
     }
-    return result.rows[0];
-  };
+  }
+
+  if (!validSortByOrders.includes(order)) {
+    throw {
+      status: 400,
+      msg: "Invalid order_by value!"
+    }
+  }
+  
+  const queryStr = `
+        SELECT 
+            articles.author, 
+            articles.title, 
+            articles.article_id, 
+            articles.topic, 
+            articles.created_at, 
+            articles.votes, 
+            articles.article_img_url,
+            COUNT(comments.comment_id)::INT AS comment_count
+        FROM articles
+        LEFT JOIN comments
+        ON articles.article_id = comments.article_id
+        GROUP BY articles.article_id
+        ORDER BY ${sort_by} ${order.toUpperCase()};
+        `;
+
+  const result = await db.query(queryStr);
+  return result.rows;
+};
+
+exports.updateArticleById = async (inc_votes, article_id) => {
+  const queryStr = `
+        UPDATE articles
+        SET votes = votes + $1
+        WHERE article_id = $2
+        RETURNING *;
+        `;
+
+  const result = await db.query(queryStr, [inc_votes, article_id]);
+
+  if (!result.rows.length) {
+    throw {
+      status: 404,
+      msg: `Article not found!`,
+    };
+  }
+  return result.rows[0];
+};
