ce76177bf487d98e4c5e3bd43477f256c77c0777
kevpstephens <kevpstephenson@gmail.com>
2025-05-02 15:49:20 +0100

    Added 16-GET /api/users/:username

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,12 +1,24 @@
-const { selectAllUsers } = require("../models/users.model")
-
+const {
+  selectAllUsers,
+  selectUserByUsername,
+} = require("../models/users.model");
 
 exports.getUsers = async (req, res, next) => {
-    try {
-        const users = await selectAllUsers()
-        res.status(200).send({users})
+  try {
+    const users = await selectAllUsers();
+    res.status(200).send({ users });
+  } catch (err) {
+    next(err);
+  }
+};
 
-    } catch (err) {
-        throw (err)
-    }
-}
+exports.getUserByUsername = async (req, res, next) => {
+  const { username } = req.params;
+
+  try {
+    const user = await selectUserByUsername(username);
+    res.status(200).send({ user });
+  } catch (err) {
+    next(err);
+  }
+};
--- app/routers/users.router.js
+++ app/routers/users.router.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getUsers } = require("../controllers/users.controller");
+const { getUsers, getUserByUsername } = require("../controllers/users.controller");
 
 const usersRouter = express.Router();
 
 // GET all users
 usersRouter.get("/", getUsers);
+usersRouter.get("/:username", getUserByUsername)
 
 module.exports = usersRouter;
