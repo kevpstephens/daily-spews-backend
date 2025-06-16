4fcaf676853467880d695c2da031b1eedb3f8de6
kevpstephens <kevpstephenson@gmail.com>
2025-06-16 15:12:15 +0100

    Revamped production-data - separated from dev-data.

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,19 +1,25 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({path: `${__dirname}/../../.env.${ENV}`})
+require("dotenv").config({ path: `${__dirname}/../../.env.${ENV}` });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error("PGDATABASE or DATABASE_URL not set");
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE || process.env.DATABASE_URL}`)
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+} else {
+  console.log(
+    `Connected to ${process.env.PGDATABASE || process.env.DATABASE_URL}`
+  );
 }
 
 const config = {};
 if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+  if (process.env.DATABASE_URL) {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+  } else {
+    console.warn("⚠️ No DATABASE_URL set. Falling back to PGDATABASE...");
+  }
 }
 
 module.exports = new Pool(config);
