a04a5031da9d57878061b27f36b18316f93de79b
kevpstephens <kevpstephenson@gmail.com>
2025-06-17 22:23:52 +0100

    Added topics to dev data to flesh out for frontend

--- src/db/data/development-data/topics.js
+++ src/db/data/development-data/topics.js
@@ -12,4 +12,34 @@ module.exports = [
     img_url:
       "https://images.pexels.com/photos/33242/cooking-ingredient-cuisine-kitchen.jpg?w=700&h=700",
   },
+  {
+    slug: "technology",
+    description:
+      "Exploring the latest gadgets, innovations, and breakthroughs shaping our digital future.",
+    img_url:
+      "https://daily-spews-api.onrender.com/images/topics/technology.jpeg",
+  },
+  {
+    slug: "film-tv",
+    description:
+      "News, reviews, and behind-the-scenes from the worlds of cinema and television.",
+    img_url: "https://daily-spews-api.onrender.com/images/topics/film-tv.jpeg",
+  },
+  {
+    slug: "politics",
+    description:
+      "Balanced and factual reporting on policy, leadership, and global events.",
+    img_url: "https://daily-spews-api.onrender.com/images/topics/politics.jpeg",
+  },
+  {
+    slug: "puppies",
+    description: "PUPPIES!!!!!",
+    img_url: "https://daily-spews-api.onrender.com/images/topics/puppies.jpeg",
+  },
+  {
+    slug: "health",
+    description:
+      "Insights on wellness, nutrition, and medical advancements to help you stay informed.",
+    img_url: "https://daily-spews-api.onrender.com/images/topics/health.jpeg",
+  },
 ];
