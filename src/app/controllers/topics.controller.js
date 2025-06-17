2f8d408104ef833e343b90f943411dab69989138
kevpstephens <kevpstephenson@gmail.com>
2025-06-17 01:07:06 +0100

    21-POST /api/topics

--- src/app/controllers/topics.controller.js
+++ src/app/controllers/topics.controller.js
@@ -1,9 +1,22 @@
-const { selectTopics } = require("../models/topics.model");
+const { selectTopics, insertTopic } = require("../models/topics.model");
 
+//! GET /api/topics
 exports.getTopics = async (req, res, next) => {
   try {
     const topics = await selectTopics();
     res.status(200).send({ topics });
+  } catch (err) {
+    next(err);
+  }
+};
+
+//! POST /api/topics
+exports.postTopic = async (req, res, next) => {
+  const topicData = req.body;
+
+  try {
+    const newTopic = await insertTopic(topicData);
+    res.status(201).send({ topic: newTopic });
   } catch (err) {
     next(err);
   }
--- src/app/models/topics.model.js
+++ src/app/models/topics.model.js
@@ -1,8 +1,24 @@
 const db = require("../../db/connection.js");
 
+//! GET /api/topics
 exports.selectTopics = async () => {
   const queryStr = `SELECT * FROM topics;`;
 
   const result = await db.query(queryStr);
   return result.rows;
+};
+
+//! POST /api/topics
+exports.insertTopic = async ({ slug, description }) => {
+  if (!slug || !description) {
+    throw { status: 400, msg: "Missing required fields!" };
+  }
+
+  const queryStr = `
+    INSERT INTO topics (slug, description)
+    VALUES ($1, $2)
+    RETURNING *;
+  `;
+  const result = await db.query(queryStr, [slug, description]);
+  return result.rows[0];
 };
