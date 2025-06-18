206964d467733cfbe67590c17e95971d05996060
kevpstephens <kevpstephenson@gmail.com>
2025-06-18 15:15:22 +0100

    Updated CORS logic for frontend compatability + added default avatar_url for users

--- src/app.js
+++ src/app.js
@@ -13,11 +13,28 @@ const {
 // ~~~~~~~~~~~~~~~ ROUTERS ~~~~~~~~~~~~~~~
 const apiRouter = require("./app/routes/api.routes");
 
+// ~~~~~~~~~~~~~~~ CORS ~~~~~~~~~~~~~~~
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://daily-spews-client.onrender.com",
+];
+
 // ~~~~~~~~~~~~~~~ STATIC FILES ~~~~~~~~~~~~~~~
 app.use(express.static("public")); // Serve static files from the 'public' directory
 
 // ~~~~~~~~~~~~~~~ GLOBAL MIDDLEWARE ~~~~~~~~~~~~~~~
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error(`Not allowed by CORS: ${origin}`));
+    },
+    credentials: true, // allow cookies
+  })
+);
 app.use(express.json());
 
 // ~~~~~~~~~~~~~~~ API ROUTER ~~~~~~~~~~~~~~~
