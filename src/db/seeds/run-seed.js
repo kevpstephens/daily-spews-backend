1582307cd1c9d61c34c88e91f26b21e60212d30b
kevpstephens <kevpstephenson@gmail.com>
2025-06-24 04:33:04 +0100

    commented out references to closeLogger

--- src/db/seeds/run-seed.js
+++ src/db/seeds/run-seed.js
@@ -19,7 +19,7 @@ console.log(
 const runSeed = () => {
   return seed(data).then(() => {
     console.log("🚫 Now closing DB connection...");
-    db.closeLogger();
+    // db.closeLogger();
     db.end();
   });
 };
