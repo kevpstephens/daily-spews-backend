5fb2c281354cba41ba7a6c136d5856c32624cb7a
kevpstephens <kevpstephenson@gmail.com>
2025-07-07 16:18:02 +0100

    Refactor Controllers: Improve Code Quality, Logging & Error Handling

--- src/app/controllers/api.controller.js
+++ src/app/controllers/api.controller.js
@@ -1,6 +1,10 @@
 const endpoints = require("../../../endpoints.json");
 
 //! GET /api
-exports.getApi = (req, res) => {
-  res.status(200).send({ endpoints });
+exports.getApi = (req, res, next) => {
+  try {
+    return res.status(200).send({ endpoints });
+  } catch (err) {
+    return next(err);
+  }
 };
